cb8ce30b53aaba1ee39ffe38283644f8bd2f718b
Tạ Quang Khôi <khoi0941@gmail.com>
2020-05-30 13:01:59 +0700

    add new console app learn C# with Microsoft

---
+++ Csharp-with-Microsoft/learn-Csharp-microsoft.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Csharp_with_Microsoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my friends");
+            string aFriend1 = "ThanhSon";
+            string aFriend2 = "KhacThinh";
+            Console.WriteLine($"My friends are {aFriend1} and {aFriend2}");
+            
+        }
+    }
+}
