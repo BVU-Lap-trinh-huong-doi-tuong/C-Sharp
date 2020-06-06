a361d8008e50e9587b125698fad469f55d51d5ad
Tạ Quang Khôi <khoi0941@gmail.com>
2020-06-06 20:56:24 +0700

    day for learn variable..

--- Csharp-with-Microsoft/learn-Csharp-microsoft.cs
+++ Csharp-with-Microsoft/learn-Csharp-microsoft.cs
@@ -16,6 +16,42 @@ namespace Csharp_with_Microsoft
             string whatisL = "    She is NGOC    ";
             Console.WriteLine($"[{whatisL}]");
             Console.WriteLine("Who is she");
+
+            string greeting = "      Hello World!       ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            int groupone;
+            groupone = 1;
+            int grouptwo;
+            grouptwo = groupone + 1;
+            
+            // int => long => float => double
+
+            int a = 10;
+            float b = 20.055f;
+            int totalINT = (int)(a + b);
+            System.Console.WriteLine(totalINT);
+            float total = a + b;
+            System.Console.WriteLine("số thực là " + total);
+
+            int k = 3, n = 4;
+            System.Console.WriteLine(1.0*k + n);
+            int x = 1, y = 2;
+            float divF = 1.0f*x / y;
+            double divD = 1.0*x / y;
+            System.Console.WriteLine(divF + " " + divD);
+
+            y--;
+            System.Console.WriteLine(y);
         }
     }
 }
