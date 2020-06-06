1a95b49d547201aa279236e127cacc0d049ac92c
Tạ Quang Khôi <khoi0941@gmail.com>
2020-06-06 14:02:54 +0700

    Who is she

--- Csharp-with-Microsoft/learn-Csharp-microsoft.cs
+++ Csharp-with-Microsoft/learn-Csharp-microsoft.cs
@@ -15,6 +15,7 @@ namespace Csharp_with_Microsoft
 
             string whatisL = "    She is NGOC    ";
             Console.WriteLine($"[{whatisL}]");
+            Console.WriteLine("Who is she")
         }
     }
 }
