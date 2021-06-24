ac758e41d609b1133d9af48ac76ecbc28d4f767f
Keios-VS2019 <taquangkhoi@hotmail.com>
2021-06-24 10:14:09 +0700

    DONE class Xe, XeMay/Oto/Tai - Kế thừa

--- QuanLyXe/Xe.cs
+++ QuanLyXe/Xe.cs
@@ -8,6 +8,50 @@ namespace QuanLyXe
 {
     class Xe
     {
-
+        protected string SoXe;
+        protected string MauXe;
+        protected string NuocSanXuat;
+        protected string HangSanXuat;
+        protected int NamSanXuat;
+        protected string SoBanhXe;
+        public Xe() // ()
+        {
+            this.SoXe = "";
+            this.MauXe = "";
+            this.NuocSanXuat = "";
+            this.HangSanXuat = "";
+            this.NamSanXuat = 0;
+            this.SoBanhXe = "";
+        }
+        public Xe
+(string SoXe, string MauXe, string NuocSanXuat, string HangSanXuat, int NamSanXuat, string SoBanhXe)
+        { // (askjhdkhsakdnksan)
+            this.SoXe = SoXe;
+            this.MauXe = MauXe;
+            this.NuocSanXuat = NuocSanXuat;
+            this.HangSanXuat = HangSanXuat;
+            this.SoBanhXe = SoBanhXe;
+        }
+        public void NhapThongTin()
+        {
+            Console.WriteLine("SoXe la: ");
+            SoXe = Console.ReadLine();
+            Console.WriteLine("MauXe la: ");
+            MauXe = Console.ReadLine();
+            Console.WriteLine("NuocSanXuat la: ");
+            NuocSanXuat = Console.ReadLine();
+            Console.WriteLine("HangSanXuat la: ");
+            HangSanXuat = Console.ReadLine();
+            Console.WriteLine("SoBanhXe la: ");
+            SoBanhXe = Console.ReadLine();
+        }
+        public void XuatThongTin()
+        {
+            Console.WriteLine("SoXe la: " + SoXe);
+            Console.WriteLine("MauXe la: " + MauXe);
+            Console.WriteLine("NuocSanXuat la: " + NuocSanXuat);
+            Console.WriteLine("HangSanXuat la: " + HangSanXuat);
+            Console.WriteLine("SoBanhXe la: " + SoBanhXe);
+        }
     }
 }
