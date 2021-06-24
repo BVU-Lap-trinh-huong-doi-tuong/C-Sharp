24825a1036b53e786e42d24688030a7b592b3519
Keios-VS2019 <taquangkhoi@hotmail.com>
2021-06-24 09:08:35 +0700

    Project QuanLyXe học Kế thừa trong C#

---
+++ QuanLyXe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuanLyXe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
